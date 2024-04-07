9e241a6b8ba8c8437b8f6c5e088a5e1bafbb1b96
kiko2183 <romankulicnij@gmail.com>
2024-04-07 16:23:36 +0300

    1

---
+++ js/task-3.js
@@ -0,0 +1,31 @@
+class StringBuilder {
+  #value;
+
+  constructor(values) {
+    this.#value = values;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    return (this.#value += str);
+  }
+
+  padStart(str) {
+    return (this.#value = str + this.#value);
+  }
+
+  padBoth(str) {
+    return (this.#value = str + this.#value + str);
+  }
+}
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
